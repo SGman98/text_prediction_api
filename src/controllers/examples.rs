f6625a2aad76cabdb080b0c218f49bc88c97d60a
SGman98 <bastiangarnica@gmail.com>
2023-06-18 14:41:53 -0500

    test: add tests for examples controller

--- src/controllers/examples.rs
+++ src/controllers/examples.rs
@@ -22,3 +22,46 @@ async fn greet(path: web::Path<GreetingPath>) -> impl Responder {
 async fn ping() -> impl Responder {
     HttpResponse::Ok().json(json!({ "message": "pong" }))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use actix_web::test;
+
+    #[actix_web::test]
+    async fn test_greet() {
+        let app = test::init_service(actix_web::App::new().configure(register_routes)).await;
+
+        let req = actix_web::test::TestRequest::get()
+            .uri("/examples/greetings/John")
+            .to_request();
+
+        let resp = actix_web::test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+
+        let body = actix_web::test::read_body(resp).await;
+        let body = String::from_utf8(body.to_vec()).unwrap();
+
+        assert_eq!(body, r#"{"message":"Hello John!"}"#);
+    }
+
+    #[actix_web::test]
+    async fn test_ping() {
+        let app = test::init_service(actix_web::App::new().configure(register_routes)).await;
+
+        let req = actix_web::test::TestRequest::get()
+            .uri("/examples/ping")
+            .to_request();
+
+        let resp = actix_web::test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+
+        let body = actix_web::test::read_body(resp).await;
+        let body = String::from_utf8(body.to_vec()).unwrap();
+
+        assert_eq!(body, r#"{"message":"pong"}"#);
+    }
+}
