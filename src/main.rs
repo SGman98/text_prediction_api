cb0186250e5034cae88a662484127e9ccc54f386
SGman98 <bastiangarnica@gmail.com>
2023-06-19 11:51:40 -0500

    feat: add cors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use actix_cors::Cors;
 use actix_web::{middleware, web, App, HttpServer};
 use log::info;
 
@@ -16,15 +17,23 @@ async fn main() -> std::io::Result<()> {
         .unwrap_or("8000".to_string())
         .parse::<u16>()
         .expect("PORT must be a number");
+    let front_url = std::env::var("FRONT_URL").expect("FRONT_URL must be set");
 
     let db_name = std::env::var("MONGO_DB").expect("MONGO_DB must be set");
     let repo = repositories::MongoRepo::init(&db_name).await;
 
     info!("Starting server on {bind_address}:{port}");
     HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin(&front_url)
+            .allow_any_method()
+            .allow_any_header()
+            .max_age(3600);
+
         App::new()
             .wrap(middleware::NormalizePath::trim())
             .wrap(middleware::Logger::default())
+            .wrap(cors)
             .app_data(web::Data::new(repo.clone()))
             .configure(controllers::register_routes)
             .route("/", web::get().to(handlers::index))
